06af46b4fff3d475e742b6a797462f9c52b06f7e
emad3020 <askerasker2016@gmail.com>
2017-12-21 00:55:59 +0200

    create profile fragment class without layout

---
+++ app/src/main/java/com/example/gm7/instaproject/Fragments/ProfileFragment.java
@@ -0,0 +1,21 @@
+package com.example.gm7.instaproject.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by emad on 12/21/17.
+ */
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
