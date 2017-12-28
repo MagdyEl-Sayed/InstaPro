50a84ae202dba431eaad8339d9cd960f284a8eca
emad3020 <askerasker2016@gmail.com>
2017-12-28 21:01:32 +0200

    Prepare Home adapter with Layout and inflater

--- app/src/main/java/com/example/gm7/instaproject/Adapter/HomeAdapter.java
+++ app/src/main/java/com/example/gm7/instaproject/Adapter/HomeAdapter.java
@@ -1,8 +1,14 @@
 package com.example.gm7.instaproject.Adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.gm7.instaproject.R;
 
 /**
  * Created by emad on 12/20/17.
@@ -10,10 +16,13 @@ import android.view.ViewGroup;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.homeViewHolder>{
 
+    private Context mContext;
 
     @Override
     public homeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        mContext = parent.getContext();
+        View rootView = LayoutInflater.from(mContext).inflate(R.layout.itemmodel_grid,parent,false);
+        return new homeViewHolder(rootView);
     }
 
     @Override
@@ -23,12 +32,19 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.homeViewHolder
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 100;
     }
 
     class homeViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mImgThumbnails;
+        private TextView mTvDescription;
+
+
+
         public homeViewHolder(View itemView) {
             super(itemView);
+            mImgThumbnails = itemView.findViewById(R.id.img_thumbnails);
+            mTvDescription = itemView.findViewById(R.id.tv_description);
         }
     }
 }
