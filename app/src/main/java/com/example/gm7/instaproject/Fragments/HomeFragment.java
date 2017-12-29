dd1d9c99797b970a01b29173b8ee5b8cb18cc958
emad3020 <askerasker2016@gmail.com>
2017-12-29 22:38:49 +0200

    And action menu to home fragment to create post

--- app/src/main/java/com/example/gm7/instaproject/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/gm7/instaproject/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.gm7.instaproject.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,9 +8,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.gm7.instaproject.Activities.CreateProjectActivity;
 import com.example.gm7.instaproject.Adapter.HomeAdapter;
 import com.example.gm7.instaproject.R;
 
@@ -28,6 +33,8 @@ public class HomeFragment extends Fragment {
 
         assert getActivity() != null;
         getActivity().setTitle(getString(R.string.menu_home));
+
+        setHasOptionsMenu(true);
     }
 
     @Nullable
@@ -41,5 +48,22 @@ public class HomeFragment extends Fragment {
         mHomeRecycler.setHasFixedSize(true);
         mHomeRecycler.setAdapter(mAdapter);
         return fragmentView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_add_project){
+            Intent intent = new Intent(getActivity(), CreateProjectActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
